460669e009d52449327386781a7e844e4b73bb69
William Bonvini <w.bonvini@reply.it>
2023-03-10 14:03:04 +0100

    modified layout

--- src/app.py
+++ src/app.py
@@ -1,106 +1,67 @@
 import pandas as pd
+import sydraw.utils.config
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 from sydraw import synth
-app = Dash(__name__)
+
+from src.slider import num_models_slider, num_points_slider, noise_perc_slider, outliers_perc_slider, radii_range_slider
+
+app = Dash(__name__,
+           meta_tags=[{'name': 'viewport',
+                       'content': 'width=device-width, initial-scale=1.0, max-scale=1.0, minimum-scale=0.9,'}
+                      ]
+           )
 
 server = app.server
 
-app.layout = html.Div([
-    dcc.Graph(id="scatter-plot",
-              style={'width': '90vh',
-                     'height': '90vh',
-                     'float': 'left',
-                     'show_scale': 'false'}),
+graph = html.Div([dcc.Graph(id="scatter-plot",
+                            style={
+                                'float': 'center',
+                            }
+                            )
+                  ],
+                 className="six columns"
+                 )
 
-    # Number of models
-    html.Div([
-        html.P("Number of models"),
-        dcc.Slider(
-            id='nm',
-            min=1, max=5, step=1,
-            marks={0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5'},
-            value=2,
-        ),
 
-        # Number of points
-        html.P("Number of points"),
-        dcc.Slider(
-                id='n',
-                min=100, max=1000, step=100,
-                marks={100: '100',
-                       200: '200',
-                       300: '300',
-                       400: '400',
-                       500: '500',
-                       600: '600',
-                       700: '700',
-                       800: '800',
-                       900: '900',
-                       1000: '1000'},
-                value=500
-            ),
+parameters = html.Div([html.H5("Number of models"),
+                       num_models_slider,
+                       html.H5("Number of points"),
+                       num_points_slider,
+                       html.H5("Noise percentage"),
+                       noise_perc_slider,
+                       html.H5("Outliers percentage"),
+                       outliers_perc_slider,
+                       html.H5("Radii range"),
+                       radii_range_slider
+                       ],
+                      className=" six columns")
 
-        # Noise
-        html.P("Noise percentage"),
-        dcc.Slider(
-                id='noise_perc',
-                min=0, max=0.1, step=0.01,
-                marks={0: '0.0',
-                       0.01: '0.01',
-                       0.02: '0.02',
-                       0.03: '0.03',
-                       0.04: '0.04',
-                       0.05: '0.05',
-                       0.06: '0.06',
-                       0.07: '0.07',
-                       0.08: '0.08',
-                       0.09: '0.09',
-                       0.1: '0.10'},
-                value=0.01
-            ),
-
-        # Outliers
-        html.P("Outliers percentage"),
-        dcc.Slider(
-                id='outliers_perc',
-                min=0, max=1, step=0.10,
-                marks={0: '0.0',
-                       0.1: '0.1',
-                       0.2: '0.2',
-                       0.3: '0.3',
-                       0.4: '0.4',
-                       0.5: '0.5',
-                       0.6: '0.6',
-                       0.7: '0.7',
-                       0.8: '0.8',
-                       0.9: '0.9',
-                       1: '1.0'},
-                value=0.1
-            ),
-        html.P("Radii range"),
-                dcc.RangeSlider(
-                        id='radius',
-                        min=0.1, max=1.0, step=0.10,
-                        marks={0: '0.0',
-                               0.1: '0.1',
-                               0.2: '0.2',
-                               0.3: '0.3',
-                               0.4: '0.4',
-                               0.5: '0.5',
-                               0.6: '0.6',
-                               0.7: '0.7',
-                               0.8: '0.8',
-                               0.9: '0.9',
-                               1: '1.0'},
-                        value=[0.3, 0.5]
-                    )
-        ],
-    style={'width': '90vh',
-           'height': '90vh',
-           'float': 'right',
-           'padding-top': '10%'})
-])
+app.layout = \
+    html.Div([html.Hr(),
+              html.Div(
+                      [
+                      html.H1("Sydraw Demo",
+                              style={'text-align': 'center'},
+                              className="six columns"
+                              ),
+                      html.Img(src="assets/sydraw.jpeg",
+                               style={"width": "5%"},
+                               className="two columns"
+                               ),
+                      html.Div([html.P(["Synthesize data corrupted by noise and outliers.",
+                                        html.Br(),
+                                       "Tweak the hyper-parameters as you wish."],
+                                       style={"text-align": "center"})],
+                               style={"margin": "0 auto"},
+                               className="four columns")
+                      ],
+                      className="row"
+                  ),
+              html.Hr(),
+              html.Div([graph, parameters], className="row"),
+              html.Hr()],
+             )
 
 
 @app.callback(
@@ -111,6 +72,9 @@ app.layout = html.Div([
     Input("outliers_perc", "value"),
     Input("radius", "value"))
 def update_bar_chart(nm, n, noise_perc, outliers_perc, radius):
+    sydraw.utils.config.OPTS["outliers"]["x_r"] = (-4.2, 4.2)
+    #sydraw.utils.config.OPTS["outliers"]["y_r"] = (-4.5, 4.5)
+
     sample = synth.circles_sample(
         nm=nm,
         n=n,
@@ -125,11 +89,10 @@ def update_bar_chart(nm, n, noise_perc, outliers_perc, radius):
         x="x",
         y="y",
         color="label",
-        range_x=(-2.5, 2.5),
-        range_y=(-2.5, 2.5),
         template='simple_white')
     fig.update_coloraxes(showscale=False)
     fig.update_traces(marker={'size': 3})
+    fig.update_layout({"yaxis": {"scaleanchor": "x"}, "xaxis": {"range": (-2.0, 2.5)}})
 
     return fig
 
